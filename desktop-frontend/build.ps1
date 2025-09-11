e9bfe82106482ed1e85c015713f36231f4831f77
Nikolay Peshev <nikolay.peshev@polygran.de>
2025-09-11 13:40:44 +0300

    add helm chart and build files

---
+++ desktop-frontend/build.ps1
@@ -0,0 +1,10 @@
+# Format: dd-MM-yy
+$DATE = Get-Date -Format "dd-MM-yy"
+
+$IMAGE_NAME = "nikolaypeshev/dogpass:edog-frontend-$DATE"
+
+Write-Host ">>> Building Docker image: $IMAGE_NAME"
+docker build -t $IMAGE_NAME .
+
+Write-Host ">>> Pushing Docker image: $IMAGE_NAME"
+docker push $IMAGE_NAME
